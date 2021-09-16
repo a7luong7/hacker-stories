e3e5502b8df50b825eb85f5d87394526ee87224d
a7luong7 <samcheung992@gmail.com>
2021-09-16 15:35:32 -0700

    Added tests for chip and icon components

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -2,17 +2,19 @@ import { render, fireEvent } from '@testing-library/react';
 import { Button } from '.'
 
 describe('Button tests', ()=>{
-    
     const labelText = "Button text";
+    const handleClick = jest.fn();
+    let component;
+
+    beforeEach(()=>{
+        component = render(<Button onClick={handleClick}>{labelText}</Button>)
+    })
     
     it('Button renders children', () => {
-        const component = render(<Button>{labelText}</Button>)
         expect(component.container).toHaveTextContent(labelText);
     })
     
     it('onclick event is fired', () => {
-        const handleClick = jest.fn();
-        const component = render(<Button onClick={handleClick}>{labelText}</Button>)
         fireEvent.click(component.getByText(labelText));
         expect(handleClick.mock.calls).toHaveLength(1)
         fireEvent.click(component.getByText(labelText));
